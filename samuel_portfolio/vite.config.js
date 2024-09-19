a68b5bae4c1026b4c3eade229fcdf8cb71ee1c1b
Samuel Molele <samuelmolele22@gmail.com>
2024-09-19 13:12:12 +0200

    Updated vite config file

--- samuel_portfolio/vite.config.js
+++ samuel_portfolio/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-    '@': '/src', 
-  },
-  server: {
-    port: 3001,
+  resolve: {
+    alias: {
+      '@': '/src',  
+    },
   },
 });
+
